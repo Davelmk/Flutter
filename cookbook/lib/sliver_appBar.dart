68a999d07972c45d0238393960d9b83cb0d72b1a
Davelmk <i417528032@163.com>
2019-04-09 22:09:02 +0800

    sliverAppBar

---
+++ cookbook/lib/sliver_appBar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  //???
+  MyApp({Key key}):super(key :key);
+  @override
+  Widget build(BuildContext context) {
+    final title="Floating AppBar";
+    return MaterialApp(
+      title: title,
+      home:Scaffold(
+        body:CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              title: Text(title),
+              floating: true,
+              flexibleSpace: Placeholder(),
+              expandedHeight: 200,
+            ),
+            //SliverGrid
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context,index)=>ListTile(title: Text("Item $index")),
+                childCount: 1000
+              ),
+            )
+          ],
+        )
+      )
+    );
+  }
+}
